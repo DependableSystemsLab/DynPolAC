12d51d96425dfd653f2f83793182a6e33c8d4eba
Mehdi <10160868+mbiuki@users.noreply.github.com>
2018-09-17 19:02:09 -0700

    not needed

--- parsePolicy/manifest.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-# a manifest format should be:
-#manifest = [{'name' : '<binary 1>', 'version' : 'binary 1 version'},
-#            {'name' : '<binary 2>', 'version' : 'binary 2 version'}]
-
-manifest = []
