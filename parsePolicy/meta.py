e022d34fb6753faff6faa1c00dd954c8a4f9dc72
Mehdi <10160868+mbiuki@users.noreply.github.com>
2018-09-17 19:02:23 -0700

    not needed

--- parsePolicy/meta.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-from datetime import date
-meta={ "identifier":"0008",
-       "version":"v0.1",
-       "name":"defdp",
-       "owner":"Mehdi Karimi",
-       "description":"Dynamic data point and policy creation from an XML fomatted files.",
-       "date":str(date.today()),
-       "tags":"application,minicloud" }
